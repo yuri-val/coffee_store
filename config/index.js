94fa29e8a11e674eacbc8657f7827f8d7c4881ff
Yuri V <yuri.valigursky@gmail.com>
2019-04-06 00:52:42 +0300

    deploy

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: (process.env.GHPAGES === '1' ? 'coffee_store/' : '') + 'static',
     assetsPublicPath: '/',
 
     /**
--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+GHPAGES=1 npm run build
 
 # navigate into the build output directory
 cd dist
